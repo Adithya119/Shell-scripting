acd82ee8819f73105106c94120306cb2cbeaa42a
Adi <kariveda11@gmail.com>
2023-08-12 15:48:18 +0530

    shell scripting notes

--- 3/positional_arg.sh
+++ 3/positional_arg.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello ${1} from ${2}. Welcome to bash scripting."
+
+
+
+#   ./positional_arg.sh Adithya Bhagyanagar
+#   Hello Adithya from Bhagyanagar. Welcome to bash scripting.
