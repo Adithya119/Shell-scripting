ab36a2ddd7a23b2f7ba8e0c09015dc10fd146911
Adi <kariveda11@gmail.com>
2023-08-12 14:46:25 +0530

    shell scripting notes

--- 2/positional_arguments.sh
+++ 2/positional_arguments.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 echo ${1}
-echo 0 = ${0} and 1 = ${1}
+echo 0 = ${0} and 1 = ${1}                # you can also include this in quotations ""
+
+
+####################  command ran on CLI #######################
+#    ./positional_arguments.sh "explained by adithya"
+#    explained by adithya                                                 
+#    0 = ./positional_arguments.sh and 1 = explained by adithya
+
+
+# ./positional_arguments ----> executable
+# "explained by adithya" ----> arguement/parameter
+
+# Note:- if "explained by adithya" was given without "" it would have taken explained as 1st arg, by as 2nd arg & adithya as 4th arg
