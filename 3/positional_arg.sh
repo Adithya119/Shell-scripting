8427c0ddeea1e6eba15be6e76cc0b9e5b28aac26
Adi <kariveda11@gmail.com>
2023-08-12 15:53:36 +0530

    shell scripting notes

--- 3/positional_arg.sh
+++ 3/positional_arg.sh
@@ -6,3 +6,7 @@ echo "Hello ${1} from ${2}. Welcome to bash scripting."
 
 #   ./positional_arg.sh Adithya Bhagyanagar
 #   Hello Adithya from Bhagyanagar. Welcome to bash scripting.
+
+# In blog, trainer has defined the args like below, but I found it's not needed.
+# name=$1
+# location=$2
