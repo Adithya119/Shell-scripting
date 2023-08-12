005dc1778cc3149df8566182d51dc404f0564c31
Adi <kariveda11@gmail.com>
2023-08-12 16:23:39 +0530

    shell scripting notes

--- 3/positional_arg.sh
+++ 3/positional_arg.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
+# name=$1
+# location=$2            ---------->                         # refer blog --> "Difference between Parameters & arguments:" sub-heading
 echo "Hello ${1} from ${2}. Welcome to bash scripting."
 
 
 
 #   ./positional_arg.sh Adithya Bhagyanagar
 #   Hello Adithya from Bhagyanagar. Welcome to bash scripting.
-
-# In blog, trainer has defined the args like below, but I found it's not needed.
-# name=$1
-# location=$2
