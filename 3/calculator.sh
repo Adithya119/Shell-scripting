29b94859ca68857e139533ac7dc860ea5a56940c
Adi <kariveda11@gmail.com>
2023-08-12 18:59:24 +0530

    shell scripting notes

--- 3/calculator.sh
+++ 3/calculator.sh
@@ -4,7 +4,7 @@ echo "calculator which can add, sub, mul & div 2 numbers"
 read -p "enter a " a
 read -p "enter b " b
 read -p "enter operation " operation
-echo $(( a ${operation} b ))                  # user echo to print to screen
+echo $(( a ${operation} b ))                 # use echo to print to screen  # this 1 line is the only login in this file --> $(( a+b )) as seen in folder 2/arithmetic.sh
 
 
 
