d2afd330b81be1f0f35fcca4b2dc9865f5270fe2
Adi <kariveda11@gmail.com>
2023-08-12 18:41:20 +0530

    shell scripting notes

---
+++ 3/user_input.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "What is your name? " your_name
+read -p "What model are your running shoes? " shoes
+read -p "What model is your bike? " bike
+
+echo "Hi $your_name, great that you have choosen $shoes for running & $bike for cycling"           #   ${your_name} also works!
