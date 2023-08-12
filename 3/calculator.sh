d3c45780acf7c7d1fcaa1debbd0e789c8601a280
Adi <kariveda11@gmail.com>
2023-08-12 18:55:13 +0530

    shell scripting notes

---
+++ 3/calculator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "calculator which can add, sub, mul & div 2 numbers"
+read -p "enter a " a
+read -p "enter b " b
+read -p "enter operation" operation
+echo $(( a ${operation} b ))                  # user echo to print to screen
+
+
+
+
+
+# ./calculator.sh
+# enter any of a+b, a-b, a*b, a/b   # just echo
+# enter a 10
+# enter b 2
+# enter operation +
+
+
