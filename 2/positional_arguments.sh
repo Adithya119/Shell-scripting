a6ae8e8ff3f1c00f3c00b357399ac96e8ca0f592
Adi <kariveda11@gmail.com>
2023-08-12 14:28:30 +0530

    shell scripting notes

---
+++ 2/positional_arguments.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo ${1}
+echo 0 = ${0} and 1 = ${1}
--- 2/text.sh
+++ 2/text.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 number=5
-echo $number + 1
+echo $number + 1             # you can also enclose the number in {} like this:--  ${number} + 1
