fc8ef15af41506c73211a6a45dbf03c7223ec202
Adi <kariveda11@gmail.com>
2023-08-14 20:02:29 +0530

    shell scripting notes

--- 4/debug-demo.sh
+++ 4/debug-demo.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-echo "The zeroth arguement (the executable) is ${0}"
+echo "The zeroth arguement (the executable) is $(basename $0)"
 echo "Hello $*"
+
+
+
+### notes:
+
+# [ec2-user@ip-172-31-45-220 4]$ ./debug-demo.sh Adithya Reddy Kariveda
+# The zeroth arguement (the executable) is ./debug-demo.sh
+# Hello Adithya Reddy Kariveda
