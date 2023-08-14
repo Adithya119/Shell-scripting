c7e4e8609bf78c63f173350f9a70c7646ceb8bba
Adi <kariveda11@gmail.com>
2023-08-14 19:47:34 +0530

    shell scripting notes

---
+++ 4/script-1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+name="Adithya RK"                                  # defining a var in script-1
+echo "This is from script-1:- name= ${name}"
+./script-2.sh                                   
+
+
+
+### notes:
+
+# 3rd statement in the code is to run script-2.sh   -->  make sure to give it's right path
---
+++ 4/script-2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "This is from script-2:- name= ${name}"
