dc1f217955fa9c855ffcd6456ab39409340da0af
Adi <kariveda11@gmail.com>
2023-08-14 19:55:19 +0530

    shell scripting notes

--- 4/script-1.sh
+++ 4/script-1.sh
@@ -2,6 +2,7 @@
 
 name="Adithya RK"                                  # defining a var in script-1
 echo "This is from script-1:- name= ${name}"
+export name                                    # export statement
 ./script-2.sh                                   
 
 
@@ -9,3 +10,5 @@ echo "This is from script-1:- name= ${name}"
 ### notes:
 
 # 3rd statement in the code is to run script-2.sh   -->  make sure to give it's right path
+
+# export statement exports the value of the variable 'name' to whichever script you mention below it
