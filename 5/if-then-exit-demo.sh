cb4ba8dffdea2c41a2a53c2b196f024477693a78
Adi <kariveda11@gmail.com>
2023-08-15 13:17:17 +0530

    shell scripting notes

--- 5/if-then-exit-demo.sh
+++ 5/if-then-exit-demo.sh
@@ -14,3 +14,26 @@ exit 1
 #### notes:
 # if, the file exists, then cat the file & return a code of 0. If not, return 1
 # 'fi' marks the end of if statement
+
+# ---
+
+# 1st outcome (when file doesn't exist) :-
+
+# [ec2-user@ip-172-31-45-220 5]$ ./if-then-exit-demo.sh         ---> doesn't return anything coz there's no file to cat
+# [ec2-user@ip-172-31-45-220 5]$ echo $?
+# 1
+
+# ---
+# 2nd outcome (file exists but is empty) :-
+
+# [ec2-user@ip-172-31-45-220 5]$ ./if-then-exit-demo.sh         ---> doesn't return anything coz the file is empty
+# [ec2-user@ip-172-31-45-220 5]$ echo $?
+# 0
+
+# ---
+# 3rd outcome (file exists & has some content in it) :-
+
+# [ec2-user@ip-172-31-45-220 5]$ ./if-then-exit-demo.sh
+# Hello world!                                                ---> cats/prints the file's contents
+# [ec2-user@ip-172-31-45-220 5]$ echo $?
+# 0
