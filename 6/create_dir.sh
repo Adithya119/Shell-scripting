8ce748784d344e68c789b252f7b8eb3f8e9f1a34
Adi <kariveda11@gmail.com>
2023-08-15 21:47:57 +0530

    shell scripting notes

--- 6/create_dir.sh
+++ 6/create_dir.sh
@@ -18,3 +18,21 @@ fi
 # creating a file inside the dir
 
 touch temp-dir/1.txt                  # this line won't even be read by the bash coz the script terminates by either of the above 2 statements (exit 0 or 1) 
+
+
+#### -----
+# outcomes:-
+
+# [ec2-user@ip-172-31-45-220 6]$ ./create_dir.sh
+# directory creation successful
+
+# [ec2-user@ip-172-31-45-220 6]$ ./create_dir.sh
+# mkdir: cannot create directory ‘temp-dir’: File exists
+# directory creation failed                   ==> coz dir already exists
+
+# [ec2-user@ip-172-31-45-220 6]$ ls
+# create_dir.sh  notes  practice.sh  temp-dir
+
+# [ec2-user@ip-172-31-45-220 6]$ cd temp-dir/
+# [ec2-user@ip-172-31-45-220 temp-dir]$ ls    ==> 1.txt not created
+# [ec2-user@ip-172-31-45-220 temp-dir]$
