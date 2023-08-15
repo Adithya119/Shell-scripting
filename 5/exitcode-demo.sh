d0acd5b47b0073cb4a883755e0bb3c00b1ee70e0
Adi <kariveda11@gmail.com>
2023-08-15 12:54:12 +0530

    shell scripting notes

---
+++ 5/exitcode-demo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mktemp
+mktemp_ec=$?
+
+mkdir /home/ubuntu                   # this will fail coz /home/ubuntu already exists
+mkdir_ec=$?
+
+echo "exit code of mktemp is ${mktemp_ec}"
+echo "exit code of mkdir id ${mkdir_ec}
