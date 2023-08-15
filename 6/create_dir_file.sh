53316e0a1f1d3ff3d2d15bfb60135f1e4ee1fc68
Adi <kariveda11@gmail.com>
2023-08-15 22:01:48 +0530

    shell scripting notes

---
+++ 6/create_dir_file.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir temp-dir
+mkdir_ec=$?
+
+
+# If dir was created, create a file inside it & list the directory's contents. If not, then exit with code 1
+
+if [[ ${mkdir_ec} -eq 0 ]]; then
+   echo "directory creation successful"
+   touch temp-dir/1.txt
+   ls --recusrive
+   exit 0
+fi
+
+   echo "directory creation failed"
+   exit 1
