30a5bf4c580cb3155c153e80ccb91fdd4e77b7e7
Adi <kariveda11@gmail.com>
2023-08-15 21:31:00 +0530

    shell scripting notes

--- 6/create_dir.sh
+++ 6/create_dir.sh
@@ -3,6 +3,9 @@
 mkdir temp-dir
 mkdir_ec=$?
 
+
+# Testing if the directory was created
+
 if [[ ${mkdir_ec} -eq 0 ]]; then
    echo "directory creation successful"
    exit 0
@@ -10,3 +13,8 @@ fi
 
    echo "directory creation failed"
    exit 1
+
+
+# creating a file inside that dir
+
+touch temp-dir/1.txt
