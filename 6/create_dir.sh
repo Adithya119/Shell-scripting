2751b920f994f0f7e63ed48b505ff8212eb22118
Adi <kariveda11@gmail.com>
2023-08-15 21:45:02 +0530

    shell scripting notes

--- 6/create_dir.sh
+++ 6/create_dir.sh
@@ -8,18 +8,13 @@ mkdir_ec=$?
 
 if [[ ${mkdir_ec} -eq 0 ]]; then
    echo "directory creation successful"
+   exit 0                                  # if you don't give this, bash will go on with the rest of the script & will also print "directory creation failed"
 fi
 
    echo "directory creation failed"
-   exit 1
+   exit 1                                  # this is required coz there's no point in continuing with this script anymore.
 
 
-# creating a file inside the dir if that dir was created
+# creating a file inside the dir
 
-if [[ -d temp-dir ]]; then
-   touch temp-dir/1.txt
-   echo "file creation successful"
-fi
-
-   echo "file creation failed"
-   exit 1
+touch temp-dir/1.txt                  # this line won't even be read by the bash coz the script terminates by either of the above 2 statements (exit 0 or 1) 
