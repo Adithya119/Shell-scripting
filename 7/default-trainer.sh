af4d1255cd0e832114c2c673f5b8b4ad10c4b471
Adi <kariveda11@gmail.com>
2023-08-16 18:05:55 +0530

    shell scripting

--- 7/default-trainer.sh
+++ 7/default-trainer.sh
@@ -3,8 +3,8 @@
 name=$1
 course=$2
 
-[ -z $name ] && name="guest"                # -z stands for 'empty'
-[[ -z $course ]] && course="IT Introduction"
+[ -z $name ] && name="guest"                     # -z stands for 'empty'
+[[ -z $course ]] && course="IT Introduction"     # used double quotes here because the user provided value of $course variable has space in it (shell scripting)
 
 echo "Hello ${name}, welcome to ${course} course"
 
