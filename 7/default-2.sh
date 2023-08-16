7859bb9a8a50a66fadc6ff01f141f4860c29aeec
Adi <kariveda11@gmail.com>
2023-08-16 18:14:38 +0530

    shell scripting

---
+++ 7/default-2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+name={$1-"guest"}
+course={$2-"IT Introduction"}
+
+# at this point, the args will either have user-provided or default values
+
+echo "Hello ${name}, welcome to ${course} course"
+
+
+
+## syntax:-
+# ${parameter-"default"}     ==>  if parameter is not provided by user, use "default"
