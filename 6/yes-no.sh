9f97330295fe294c92a4cec5e599803b6ca185e4
Adi <kariveda11@gmail.com>
2023-08-16 14:08:07 +0530

    shell scripting

--- 6/yes-no.sh
+++ 6/yes-no.sh
@@ -14,3 +14,30 @@ fi
 # Notes:-
 # -eq is not working. -eq & -ne works only with numericals. Hence use =
 # enclose the y in quotations:- 'y'
+# ---
+
+# some outcomes:-
+
+# [ec2-user@ip-172-31-45-220 6]$ ./yes-no.sh
+# Do you like shell scripting? y
+# Great! continue your journey of learning
+
+# [ec2-user@ip-172-31-45-220 6]$ ./yes-no.sh
+# Do you like shell scripting? yes
+# Great! continue your journey of learning
+
+# [ec2-user@ip-172-31-45-220 6]$ ./yes-no.sh
+# Do you like shell scripting? YES
+# Great! continue your journey of learning
+
+# [ec2-user@ip-172-31-45-220 6]$ ./yes-no.sh
+# Do you like shell scripting? no
+
+# [ec2-user@ip-172-31-45-220 6]$ ./yes-no.sh
+# Do you like shell scripting? yeS               # yeS, yEs, yES
+# Great! continue your journey of learning
+
+# [ec2-user@ip-172-31-45-220 6]$ ./yes-no.sh
+# Do you like shell scripting? Yes
+# Great! continue your journey of learning
+# [ec2-user@ip-172-31-45-220 6]$
