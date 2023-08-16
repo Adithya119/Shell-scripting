56c39711b5cf63a1a0587a26c45992a4a5812725
Adi <kariveda11@gmail.com>
2023-08-16 14:03:53 +0530

    shell scripting

--- 6/yes-no.sh
+++ 6/yes-no.sh
@@ -2,7 +2,15 @@
 
 read -p "Do you like shell scripting? " reply
 
-if [[ ${reply} = 'y' ]]; then
+if [[ ${reply,,} = 'y' ]] || [[ ${reply^^} = 'YES' ]]; then               # combining 2 stmts with 'or'                
    echo "Great! continue your journey of learning"
    exit 0
 fi
+
+
+
+# ----------
+
+# Notes:-
+# -eq is not working. -eq & -ne works only with numericals. Hence use =
+# enclose the y in quotations:- 'y'
