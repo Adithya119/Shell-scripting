c7546bb22528d91bea0f0cbd4dd8d02fac08d1cb
Adi <kariveda11@gmail.com>
2023-08-16 12:38:45 +0530

    shell scripting

--- 6/if-then-else-arg.sh
+++ 6/if-then-else-arg.sh
@@ -20,3 +20,16 @@ else
    echo "printing the contents of ${file_name} below:-"
    cat ${file_name}
 fi
+
+
+
+##### ----------------
+# outcome:-
+
+# [ec2-user@ip-172-31-45-220 6]$ ./if-then-else-arg.sh
+# file name is not given
+# [ec2-user@ip-172-31-45-220 6]$ ./if-then-else-arg.sh /tmp/4.txt
+# /tmp/4.txt not found
+# [ec2-user@ip-172-31-45-220 6]$ ./if-then-else-arg.sh /tmp/2.txt
+# printing the contents of /tmp/2.txt below:-
+# line1
