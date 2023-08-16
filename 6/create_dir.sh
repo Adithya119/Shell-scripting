d94ec73346f5a25317f922cfd7c0187c3d1f29a8
Adi <kariveda11@gmail.com>
2023-08-16 10:50:47 +0530

    shell scripting notes

--- 6/create_dir.sh
+++ 6/create_dir.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# trainer has written this script in a different way ==> using "-ne 0"
 
 mkdir temp-dir
 mkdir_ec=$?
@@ -38,5 +39,3 @@ touch temp-dir/1.txt                  # this line won't even be read by the bash
 # [ec2-user@ip-172-31-45-220 temp-dir]$
 
 #    ----------
-
-# trainer has written this script in a different way ==> using "-ne 0"
