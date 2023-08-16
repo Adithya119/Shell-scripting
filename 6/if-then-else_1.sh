6cf2f936675415460bc0cef773a6f1bd9f9d57d2
Adi <kariveda11@gmail.com>
2023-08-16 10:52:18 +0530

    shell scripting notes

--- 6/if-then-else_1.sh
+++ 6/if-then-else_1.sh
@@ -4,9 +4,9 @@ FILE=/tmp/2.txt
 
 # check if file exists
 
-if [[ -f ${{FILE}} ]]; then
+if [[ -f ${FILE} ]]; then
    echo "printing the contents of ${FILE} below:- "
-   cat ${{ FILE }}
+   cat ${ FILE }
 
 else 
    echo "file ${FILE} doesn't exist"
