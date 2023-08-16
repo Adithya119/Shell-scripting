964bd9444d52e7f954cc842822f2f7d03f2fb66f
Adi <kariveda11@gmail.com>
2023-08-16 19:08:14 +0530

    shell scripting

--- 7/IFS-1.sh
+++ 7/IFS-1.sh
@@ -2,7 +2,11 @@
 
 file_name=test-1.txt
 
-for item in $(cat ${file_name})
+for item in $(cat ${file_name})        # for every item in the contents of the given file
 do
    echo $item
 done
+
+
+### Notes:-
+# you are storing each content in 'item', hence you need to use $item while printing
