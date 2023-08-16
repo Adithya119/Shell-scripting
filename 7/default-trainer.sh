253429b25bf3d2f808276ecf4055fa153bd6f6db
Adi <kariveda11@gmail.com>
2023-08-16 18:03:19 +0530

    shell scripting

--- 7/default-trainer.sh
+++ 7/default-trainer.sh
@@ -4,6 +4,10 @@ name=$1
 course=$2
 
 [ -z $name ] && name="guest"                # -z stands for 'empty'
-[ -z $course ] && course="IT Introduction"
+[[ -z $course ]] && course="IT Introduction"
 
 echo "Hello ${name}, welcome to ${course} course"
+
+
+
+# this script is also giving the same outcomes as default-1.sh
