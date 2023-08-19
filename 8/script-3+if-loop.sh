d9b5bcdf3ecdaa913ec55163698d729cfa19440c
Adi <kariveda11@gmail.com>
2023-08-19 15:15:58 +0530

    shell scripting

--- 8/script-3+if-loop.sh
+++ 8/script-3+if-loop.sh
@@ -4,7 +4,7 @@ dir_path=${1-"/home/ec2-user/contents/"}
 
 for item in $(ls ${dir_path})
 do
-   echo "processing ${path}"
+   echo "processing ${item}"
    if [ -d "${item}" ]; then
       echo "${item} is a directory"
    elif [ -f "${item}" ]; then
