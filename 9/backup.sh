11125ba9c8844da8f830d64d9b6bd1e360ea705b
Adi <kariveda11@gmail.com>
2023-08-21 10:02:47 +0530

    shell scripting

--- 9/backup.sh
+++ 9/backup.sh
@@ -4,7 +4,7 @@
 # checking if file name is given as an arguement.
 
 is_file() {
-    if [ ! -f "$1" ]; then
+    if [ ! -f "${1}" ]; then
         echo "please enter a file next to ${0}. Cannot find ${1}"
         exit 2
     fi
@@ -13,7 +13,7 @@ is_file() {
 # back up the file which was given in "${1}"
 
 backup_file() {
-    is_file "$1"
+    is_file "${1}"
     new_file_loc="${1}.bak" 
     cp $1 $new_file_loc
     echo "file is copied to $new_file_loc"
