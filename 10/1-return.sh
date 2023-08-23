8e693d2d5d5dd0e2949e740bfe7c6f7d4a125868
Adi <kariveda11@gmail.com>
2023-08-23 18:06:36 +0530

    shell scripting

--- 10/1-return.sh
+++ 10/1-return.sh
@@ -11,7 +11,10 @@ VALUE=$( getValue "12345" )
 echo $VALUE
 
 
+
 # ----
 
 # [ec2-user@ip-172-31-45-220 10]$ ./1-return.sh
 # something12345
+
+# All this script did was to pass whatever the function has returned, to a variable called as VALUE & echoed it.
