04425bc63dc018c01557a5d0eb32460d7e05d462
Adi <kariveda11@gmail.com>
2023-08-23 17:15:55 +0530

    shell scripting

--- 10/vars-conflict.sh
+++ 10/vars-conflict.sh
@@ -5,7 +5,7 @@ echo "value of var_1 -- ${var_1}"
 echo "..."
 
 function_test() {
-    var_1=100
+    var_1=100       # the same var re-defined with new value
     var_2=2
 }
 
@@ -14,3 +14,12 @@ function_test
 
 echo "value of var_1 -- ${var_1}"
 echo "value of var_2 -- ${var_2}"
+
+
+echo "..."
+
+# the same vars re-defined with new values
+var_1=200
+var_2=300
+echo "value of var_1 -- ${var_1}"
+echo "value of var_1 -- ${var_1}"
