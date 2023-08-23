411fcf9e0b76b078cc5b6d2afe724b508b560a91
Adi <kariveda11@gmail.com>
2023-08-23 17:13:21 +0530

    shell scripting

--- 10/variable-scope.sh
+++ 10/variable-scope.sh
@@ -11,3 +11,12 @@ function_test
 
 echo "value of var_1 -- ${var_1}"
 echo "value of var_2 -- ${var_2}"
+
+# --
+
+# vars defined inside a function or outside are global vars, by default:-
+
+# [ec2-user@ip-172-31-45-220 10]$ ./variable-scope.sh
+# calling function
+# value of var_1 -- 1
+# value of var_2 -- 2
---
+++ 10/vars-conflict.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+var_1=1
+echo "value of var_1 -- ${var_1}"
+echo "..."
+
+function_test() {
+    var_1=100
+    var_2=2
+}
+
+echo "calling function"
+function_test
+
+echo "value of var_1 -- ${var_1}"
+echo "value of var_2 -- ${var_2}"
