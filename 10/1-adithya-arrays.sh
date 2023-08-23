6a3585c3ed1d8f28225fbb78943fed45774dd08e
Adi <kariveda11@gmail.com>
2023-08-23 12:18:41 +0530

    shell scripting

--- 10/1-adithya-arrays.sh
+++ 10/1-adithya-arrays.sh
@@ -4,8 +4,10 @@ names=(Gomathi Adithya twins)
 
 echo "array has the following name(s): ${names[@]}"
 
-display_names "${names[@]}"
-
 display_names() {
     echo "array has the following name(s) inside function: ${names[@]}"
 }
+
+echo "Calling function"
+
+display_names "${names[@]}"
