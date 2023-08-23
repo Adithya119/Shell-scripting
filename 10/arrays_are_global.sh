14c009eb05c4f5ccad419d0a73227d0e77f3f57b
Adi <kariveda11@gmail.com>
2023-08-23 16:40:37 +0530

    shell scripting

--- 10/arrays_are_global.sh
+++ 10/arrays_are_global.sh
@@ -2,11 +2,21 @@
 
 
 display_names() {
-    test_array=(test-1 test-2)
+    local test_array=(test-1 test-2)
     echo "values of test_array inside function: ${test_array[*]}"
-    echo "${1}"         # will display the 1st arg that wes passed to the function while calling it
+    echo "${1}"         # will display the 1st arg that was passed to the function while calling it.
 }
 
 display_names
 
 echo "values of test_array outside function: ${test_array[*]}"
+
+
+
+
+# ---
+
+# [ec2-user@ip-172-31-45-220 10]$ ./arrays_are_global.sh
+# values of test_array inside function: test-1 test-2
+#                                             -----------------> empty here coz no arg was passed to the function while calling it.                                                          
+# values of test_array outside function: test-1 test-2
