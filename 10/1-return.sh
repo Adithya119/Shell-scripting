423c929ffd309a654a6b8209595c12679cd5d918
Adi <kariveda11@gmail.com>
2023-08-23 17:54:17 +0530

    shell scripting

--- 10/1-return.sh
+++ 10/1-return.sh
@@ -4,22 +4,8 @@ getValue()
 {
     ABC=$1
     XYZ="something"$ABC
-    return "$XYZ"
+    echo $XYZ
 }
 
 VALUE=$( getValue "12345" )
 echo $VALUE
-
-
-
-# ---------------
-# getValue()
-# {
-#     ABC=$1
-#     XYZ="something"$ABC
-#     echo $XYZ
-# }
-# 
-# 
-# VALUE=$( getValue "12345" )
-# echo $VALUE
