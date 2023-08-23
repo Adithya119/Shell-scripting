1835ec891efd749fb1e07e9dc9dd4946df03aa7c
Adi <kariveda11@gmail.com>
2023-08-23 13:29:20 +0530

    shell scripting

--- 10/4-adithya-arrays.sh
+++ 10/4-adithya-arrays.sh
@@ -2,10 +2,10 @@
 
 names=(Gomathi 'Adithya RK' twins)
 echo "array has the following name(s): ${names[*]}"
-echo "1st parameter: ${names[0]}"    ##
-echo "2nd parameter: ${names[1]}"
-echo "3rd parameter: ${names[2]}"
-echo "4th parameter: ${names[3]}"
+echo "0th parameter: ${names[0]}"    ##
+echo "1st parameter: ${names[1]}"
+echo "2nd parameter: ${names[2]}"
+echo "3rd parameter: ${names[3]}"
 
 for item in "${names[@]}"
 do
@@ -15,10 +15,10 @@ done
 display_names() {
     array=$@
     echo "array has the following name(s) inside function: $@"   ##
-    echo "1st parameter: ${0}"   ##
-    echo "2nd parameter: ${1}"
-    echo "3rd parameter: ${2}"
-    echo "4th parameter: ${3}"
+    echo "0th parameter: ${0}"   ##
+    echo "1st parameter: ${1}"
+    echo "2nd parameter: ${2}"
+    echo "3rd parameter: ${3}"
 
     for item in "$@"
     do
