a49ef02c868b278ca3654df4f545cd1fc53f3e3e
Adi <kariveda11@gmail.com>
2023-08-23 16:50:00 +0530

    shell scripting

---
+++ 10/array_conflict.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+test_array=(test-1 test-2)
+echo "values of test_array outside function: ${test_array[*]}"
+
+display_names() {
+    test_array=(test-1 test-2)
+    echo "values of test_array inside function: ${test_array[*]}"
+}
+
+display_names
+
+echo "values of test_array outside function: ${test_array[*]}"
--- 10/local_arrays.sh
+++ 10/local_arrays.sh
@@ -2,7 +2,7 @@
 
 
 display_names() {
-    local test_array=(test-1 test-2)
+    local test_array=(test-1 test-2)    # the use of 'local' keyword before the array name does the trick
     echo "values of test_array inside function: ${test_array[*]}"      
 }
 
@@ -13,3 +13,8 @@ echo "values of test_array outside function: ${test_array[*]}"
 
 
 # ---
+
+# [ec2-user@ip-172-31-45-220 10]$
+# [ec2-user@ip-172-31-45-220 10]$ ./local_arrays.sh
+# values of test_array inside function: test-1 test-2
+# values of test_array outside function:
