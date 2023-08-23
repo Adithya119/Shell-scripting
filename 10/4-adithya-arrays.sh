79c09532f39b2def883f41b1822beb2048b9c5b5
Adi <kariveda11@gmail.com>
2023-08-23 13:31:45 +0530

    shell scripting

--- 10/4-adithya-arrays.sh
+++ 10/4-adithya-arrays.sh
@@ -5,7 +5,7 @@ echo "array has the following name(s): ${names[*]}"
 echo "0th parameter: ${names[0]}"    ##
 echo "1st parameter: ${names[1]}"
 echo "2nd parameter: ${names[2]}"
-echo "3rd parameter: ${names[3]}"
+echo "3rd parameter: ${names[3]}" || echo "empty"
 
 for item in "${names[@]}"
 do
@@ -34,3 +34,21 @@ display_names "${names[@]}"
 
 # --------
 
+# [ec2-user@ip-172-31-45-220 10]$ ./4-adithya-arrays.sh
+# array has the following name(s): Gomathi Adithya RK twins
+# 0th parameter: Gomathi
+# 1st parameter: Adithya RK
+# 2nd parameter: twins
+# 3rd parameter:              ---------->   empty
+# name is: Gomathi
+# name is: Adithya RK
+# name is: twins
+# Calling function
+# array has the following name(s) inside function: Gomathi Adithya RK twins
+# 0th parameter: ./4-adithya-arrays.sh    ---------->  executable is $0
+# 1st parameter: Gomathi
+# 2nd parameter: Adithya RK
+# 3rd parameter: twins
+# name inside function is: Gomathi
+# name inside function is: Adithya RK
+# name inside function is: twins
