122bc82bf2580fd3d952c9fab179c06239bf6012
Adi <kariveda11@gmail.com>
2023-08-23 13:09:57 +0530

    shell scripting

--- 10/2-adithya-arrays.sh
+++ 10/2-adithya-arrays.sh
@@ -9,10 +9,10 @@ do
 done
 
 display_names() {
-    array=$*   # 2
+    array=$@   # 2
     echo "array has the following name(s) inside function: ${array[@]}"
 
-    for item in "$*"
+    for item in "$*"              # 3     # giving * will take all values of array as 1 string & hence display all values in 1 line (looping is not done properly)
     do
        echo "name inside function is: ${item}"
     done
