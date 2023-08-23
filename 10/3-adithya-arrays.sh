064ab8073266c742c4419d72125fb236ae1d011b
Adi <kariveda11@gmail.com>
2023-08-23 13:17:19 +0530

    shell scripting

--- 10/3-adithya-arrays.sh
+++ 10/3-adithya-arrays.sh
@@ -11,8 +11,8 @@ done
 display_names() {
     array=$@
     echo "array has the following name(s) inside function: ${array[@]}"
-    echo "${array[1]}"
-    
+    echo ${array[1]}
+
     for item in "$@"
     do
        echo "name inside function is: ${item}"
