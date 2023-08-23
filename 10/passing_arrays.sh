c0a18cd505e7881368b085a62bd57a1d0ebbc564
Adi <kariveda11@gmail.com>
2023-08-23 11:34:57 +0530

    shell scripting

--- 10/passing_arrays.sh
+++ 10/passing_arrays.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#display_array() {
-#    array=$@
-#    echo "The array inside the function is ${array[*]}" 
-#}
+display_array() {
+    array=$@
+    echo "The array inside the function is ${array[*]}" 
+}
 
 test_input_1=(1 2 3 4 5)
 
 echo "The orginal array for test input 1 is ${test_input_1[*]}"
 #echo "Calling function"
 
-#display_array ${test_input_1[*]}
+display_array ${test_input_1[*]}
