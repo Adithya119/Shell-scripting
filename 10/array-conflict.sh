2313fd7ae2acfc07cfe28d756c5c17b730d4c802
Adi <kariveda11@gmail.com>
2023-08-23 17:07:00 +0530

    shell scripting

--- 10/array-conflict.sh
+++ 10/array-conflict.sh
@@ -17,7 +17,7 @@ echo "values of test_array outside function: ${test_array[*]}"
 
 # ----
 
-# [ec2-user@ip-172-31-45-220 10]$ ./array_conflict.sh
+# [ec2-user@ip-172-31-45-220 10]$ ./array-conflict.sh
 # values of test_array outside function: test-1 test-2
 # values of test_array inside function: test-3 test-4
 # values of test_array outside function: test-3 test-4
---
+++ 10/variable-scope.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+var_1=1
+
+function_test() {
+    var_2=2
+}
+
+echo "value of var_1 outside -- ${var_1}"
+echo "value of var_2 outside -- ${var_2}"
