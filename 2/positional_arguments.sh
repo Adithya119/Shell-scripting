3802194912e54528e43db6a83639027ff056237a
Adi <kariveda11@gmail.com>
2024-01-05 23:23:42 +0530

    comments

--- 2/positional_arguments.sh
+++ 2/positional_arguments.sh
@@ -13,4 +13,4 @@ echo 0 = ${0} and 1 = ${1}                # you can also include this in quotati
 # ./positional_arguments ----> executable
 # "explained by adithya" ----> arguement/parameter
 
-# Note:- if "explained by adithya" was given without "" it would have taken explained as 1st arg, by as 2nd arg & adithya as 4th arg
+# Note:- if "explained by adithya" was given without "" it would have taken explained as 1st arg, by as 2nd arg & adithya as 3rd arg
--- 3/calculator.sh
+++ 3/calculator.sh
@@ -4,7 +4,7 @@ echo "calculator which can add, sub, mul & div 2 numbers"
 read -p "enter a " a                         # use read to prompt for user input & use -p to store the user input in some variable (in this case, I chose 'a')
 read -p "enter b " b
 read -p "enter operation " operation
-echo $(( a ${operation} b ))                 # use echo to print to screen  # this 1 line is the only login in this file --> $(( a + b )) as seen in folder 2/arithmetic.sh
+echo $(( $a ${operation} $b ))                 # use echo to print to screen  # this 1 line is the only login in this file --> $(( a + b )) as seen in folder 2/arithmetic.sh
 
 
 
